900aff8be43731837c15461951cb1b9bd6b99d71
Adnan Zahid <adnaan.zaahid@gmail.com>
2016-04-06 22:00:59 +0500

    Initial Commit

---
+++ Chess/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Chess
+//
+//  Created by Adnan Zahid on 4/6/16.
+//  Copyright © 2016 Adnan Zahid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
